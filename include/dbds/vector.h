d153bc98ac06826f02a9de9378ad02b9c3b265dd
David Brito <39559632+davbrito@users.noreply.github.com>
2020-12-12 15:43:21 -0400

    fix vector interface

--- include/dbds/vector.h
+++ include/dbds/vector.h
@@ -13,6 +13,7 @@ typedef struct {
   void* data;
   size_t size;
   size_t capacity;
+  size_t value_size;
 } dbds_vector;
 
 /// Create a new empty vector with an initial preallocated capacity
@@ -22,14 +23,12 @@ DBDS_EXPORT dbds_vector dbds_vector_new(size_t capacity, size_t data_size);
 DBDS_EXPORT void dbds_vector_clear(dbds_vector*);
 
 /// Inserts an element on the end of the vector.
-DBDS_EXPORT void* dbds_vector_push_back(dbds_vector*, const void* value,
-                                        size_t data_size);
+DBDS_EXPORT void* dbds_vector_push_back(dbds_vector*, const void* value);
 
 /// Remove the element on the end of the vector.
 DBDS_EXPORT void dbds_vector_pop_back(dbds_vector*);
 
 /// Retrieves the ith element on the vector.
-DBDS_EXPORT void* dbds_vector_get(const dbds_vector*, size_t i,
-                                  size_t data_size);
+DBDS_EXPORT void* dbds_vector_get(const dbds_vector*, size_t i);
 
 #endif  // DBDS_VECTOR_H_
--- source/dbds/vector.c
+++ source/dbds/vector.c
@@ -20,18 +20,16 @@ bool vector_needs_shrink(const dbds_vector* v) {
   return v->capacity / 3 > v->size / 2;
 }
 
-void reallocate_vector(dbds_vector* v, size_t new_capacity, size_t data_size) {
-  v->data = realloc(v->data, new_capacity * data_size);
+void reallocate_vector(dbds_vector* v, size_t new_capacity) {
+  v->data = realloc(v->data, new_capacity * v->value_size);
   v->capacity = new_capacity;
 }
 
-void grow_vector(dbds_vector* v, size_t data_size) {
-  reallocate_vector(v, calculate_capacity_growth(v->capacity), data_size);
+void grow_vector(dbds_vector* v) {
+  reallocate_vector(v, calculate_capacity_growth(v->capacity));
 }
 
-void shrink_vector(dbds_vector* v, size_t data_size) {
-  reallocate_vector(v, v->size, data_size);
-}
+void shrink_vector(dbds_vector* v) { reallocate_vector(v, v->size); }
 
 DBDS_EXPORT dbds_vector dbds_vector_new(size_t capacity, size_t data_size) {
   capacity = capacity || default_initial_vector_capacity(data_size);
@@ -39,6 +37,7 @@ DBDS_EXPORT dbds_vector dbds_vector_new(size_t capacity, size_t data_size) {
       .data = malloc(capacity * data_size),
       .capacity = capacity,
       .size = 0,
+      .value_size = data_size,
   };
 }
 
@@ -51,13 +50,12 @@ DBDS_EXPORT void dbds_vector_clear(dbds_vector* v) {
 
 bool vector_needs_grow(const dbds_vector* v) { return v->capacity == v->size; }
 
-DBDS_EXPORT void* dbds_vector_push_back(dbds_vector* v, const void* value,
-                                        size_t data_size) {
-  if (v == NULL) return NULL;
-  if (vector_needs_grow(v)) grow_vector(v, data_size);
-  void* last = (unsigned char*)v->data + v->size * data_size;
+DBDS_EXPORT void* dbds_vector_push_back(dbds_vector* v, const void* value) {
+  if (!v) return NULL;
+  if (vector_needs_grow(v)) grow_vector(v);
+  void* last = (unsigned char*)v->data + v->size * v->value_size;
   v->size++;
-  return memcpy(last, value, data_size);
+  return memcpy(last, value, v->value_size);
 }
 
 DBDS_EXPORT void dbds_vector_pop_back(dbds_vector* v) {
@@ -66,8 +64,7 @@ DBDS_EXPORT void dbds_vector_pop_back(dbds_vector* v) {
   }
 }
 
-DBDS_EXPORT void* dbds_vector_get(const dbds_vector* v, size_t i,
-                                  size_t data_size) {
+DBDS_EXPORT void* dbds_vector_get(const dbds_vector* v, size_t i) {
   if (!v || !v->data || i >= v->size) return NULL;
-  return (unsigned char*)v->data + i * data_size;
+  return &((unsigned char*)v->data)[i * v->value_size];
 }
