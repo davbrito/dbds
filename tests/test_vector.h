5af86c3c824b966d3b4427c2b5d76df5258d69cb
David Brito <39559632+davbrito@users.noreply.github.com>
2020-12-12 15:45:06 -0400

    fix vector tests to use the new interface

--- tests/test_vector.h
+++ tests/test_vector.h
@@ -12,21 +12,21 @@ void test_vector() {
   const int elements = 20;
 
   for (int i = 0; i < elements; i++) {
-    const int *ptr = dbds_vector_push_back(&vec, &i, sizeof(int));
+    const int *ptr = dbds_vector_push_back(&vec, &i);
     CHECK_I("Test push back", i,
             vec.size == i + 1 && vec.capacity != 0 && vec.data != NULL);
     CHECK_I("Test push back return value", i, *ptr == i);
   }
 
   for (int i = 0; i < elements; i++) {
-    const int *ptr = dbds_vector_get(&vec, i, sizeof(int));
+    const int *ptr = dbds_vector_get(&vec, i);
     CHECK_I("Test get", i, *ptr == i);
   }
 
   for (int i = 0; i < elements; i++) {
     dbds_vector_pop_back(&vec);
     CHECK_I("Test pop back", i, vec.size == elements - i - 1);
-    int *ptr = dbds_vector_get(&vec, elements - i - 1, sizeof(int));
+    int *ptr = dbds_vector_get(&vec, elements - i - 1);
     CHECK_I("Test pop back then get", i, ptr == NULL);
   }
 
