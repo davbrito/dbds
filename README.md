543f1d3cbb1fd11d36057499f963cb3b6dd4e189
David Brito <39559632+davbrito@users.noreply.github.com>
2021-02-01 03:29:15 -0400

    CMake GIthub Actions (#1)
    * CMake GIthub Actions
    
    * Coveralls Badge on Readme
    
    * Add coverage reporting on CMakelists
    
    * Fix msvc errors.
    
    * CTest verbose.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 dbds
 ----
+[![Coverage Status](https://coveralls.io/repos/github/davbrito/dbds/badge.svg?branch=master)](https://coveralls.io/github/davbrito/dbds?branch=master)
 
 A C library of common data structures.
--- testing/include/testing/test.h
+++ testing/include/testing/test.h
@@ -6,6 +6,97 @@
 #include <stdlib.h>
 #include <string.h>
 
+// COMPILER VERSION
+// ideas for the version stuff are taken from here:
+// https://github.com/cxxstuff/cxx_detect
+
+#define TESTING_COMPILER(MAJOR, MINOR, PATCH) \
+  ((MAJOR)*10000000 + (MINOR)*100000 + (PATCH))
+
+// GCC/Clang and GCC/MSVC are mutually exclusive, but Clang/MSVC are not because
+// of clang-cl...
+#if defined(_MSC_VER) && defined(_MSC_FULL_VER)
+#if _MSC_VER == _MSC_FULL_VER / 10000
+#define TESTING_MSVC \
+  TESTING_COMPILER(_MSC_VER / 100, _MSC_VER % 100, _MSC_FULL_VER % 10000)
+#else  // MSVC
+#define TESTING_MSVC                                               \
+  TESTING_COMPILER(_MSC_VER / 100, (_MSC_FULL_VER / 100000) % 100, \
+                   _MSC_FULL_VER % 100000)
+#endif  // MSVC
+#endif  // MSVC
+#if defined(__clang__) && defined(__clang_minor__)
+#define TESTING_CLANG \
+  TESTING_COMPILER(__clang_major__, __clang_minor__, __clang_patchlevel__)
+#elif defined(__GNUC__) && defined(__GNUC_MINOR__) && \
+    defined(__GNUC_PATCHLEVEL__) && !defined(__INTEL_COMPILER)
+#define TESTING_GCC \
+  TESTING_COMPILER(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__)
+#endif  // GCC
+
+#ifndef TESTING_MSVC
+#define TESTING_MSVC 0
+#endif  // TESTING_MSVC
+#ifndef TESTING_CLANG
+#define TESTING_CLANG 0
+#endif  // TESTING_CLANG
+#ifndef TESTING_GCC
+#define TESTING_GCC 0
+#endif  // TESTING_GCC
+
+// COMPILER WARNINGS HELPERS
+
+#if TESTING_CLANG
+#define TESTING_PRAGMA_TO_STR(x) _Pragma(#x)
+#define TESTING_CLANG_SUPPRESS_WARNING_PUSH _Pragma("clang diagnostic push")
+#define TESTING_CLANG_SUPPRESS_WARNING(w) \
+  TESTING_PRAGMA_TO_STR(clang diagnostic ignored w)
+#define TESTING_CLANG_SUPPRESS_WARNING_POP _Pragma("clang diagnostic pop")
+#define TESTING_CLANG_SUPPRESS_WARNING_WITH_PUSH(w) \
+  TESTING_CLANG_SUPPRESS_WARNING_PUSH TESTING_CLANG_SUPPRESS_WARNING(w)
+#else  // TESTING_CLANG
+#define TESTING_CLANG_SUPPRESS_WARNING_PUSH
+#define TESTING_CLANG_SUPPRESS_WARNING(w)
+#define TESTING_CLANG_SUPPRESS_WARNING_POP
+#define TESTING_CLANG_SUPPRESS_WARNING_WITH_PUSH(w)
+#endif  // TESTING_CLANG
+
+#if TESTING_GCC
+#define TESTING_PRAGMA_TO_STR(x) _Pragma(#x)
+#define TESTING_GCC_SUPPRESS_WARNING_PUSH _Pragma("GCC diagnostic push")
+#define TESTING_GCC_SUPPRESS_WARNING(w) \
+  TESTING_PRAGMA_TO_STR(GCC diagnostic ignored w)
+#define TESTING_GCC_SUPPRESS_WARNING_POP _Pragma("GCC diagnostic pop")
+#define TESTING_GCC_SUPPRESS_WARNING_WITH_PUSH(w) \
+  TESTING_GCC_SUPPRESS_WARNING_PUSH TESTING_GCC_SUPPRESS_WARNING(w)
+#else  // TESTING_GCC
+#define TESTING_GCC_SUPPRESS_WARNING_PUSH
+#define TESTING_GCC_SUPPRESS_WARNING(w)
+#define TESTING_GCC_SUPPRESS_WARNING_POP
+#define TESTING_GCC_SUPPRESS_WARNING_WITH_PUSH(w)
+#endif  // TESTING_GCC
+
+#if TESTING_MSVC
+#define TESTING_MSVC_SUPPRESS_WARNING_PUSH __pragma(warning(push))
+#define TESTING_MSVC_SUPPRESS_WARNING(w) __pragma(warning(disable : w))
+#define TESTING_MSVC_SUPPRESS_WARNING_POP __pragma(warning(pop))
+#define TESTING_MSVC_SUPPRESS_WARNING_WITH_PUSH(w) \
+  TESTING_MSVC_SUPPRESS_WARNING_PUSH TESTING_MSVC_SUPPRESS_WARNING(w)
+#else  // TESTING_MSVC
+#define TESTING_MSVC_SUPPRESS_WARNING_PUSH
+#define TESTING_MSVC_SUPPRESS_WARNING(w)
+#define TESTING_MSVC_SUPPRESS_WARNING_POP
+#define TESTING_MSVC_SUPPRESS_WARNING_WITH_PUSH(w)
+#endif  // TESTING_MSVC
+
+// WARNING SUPPRESS
+TESTING_GCC_SUPPRESS_WARNING_PUSH
+TESTING_GCC_SUPPRESS_WARNING("-Wformat")
+TESTING_GCC_SUPPRESS_WARNING("-Wformat-extra-args")
+
+TESTING_CLANG_SUPPRESS_WARNING_PUSH
+TESTING_GCC_SUPPRESS_WARNING("-Wformat-extra-args")
+
 typedef struct test {
   const char* name;
   const char* file;
@@ -17,10 +108,10 @@ typedef test_function* test_function_ptr;
 
 #define MAX_TEST_DESC 100
 
-static char test_failed_description[MAX_TEST_DESC] = "";
-
 bool __test_failed();
 bool __check(const char* context, bool assertion);
+
+#define TESTING_FORMAT_TO_STR(fmt) #fmt
 
 #define TEST_DATA_VAR_NAME __test_data
 #define RETURN_TEST_CASE return &TEST_DATA_VAR_NAME
@@ -51,23 +142,29 @@ bool __check(const char* context, bool assertion);
 
 #define CHECK_I(name, i, assertion) _CHECK_2(name, assertion, "(%i)", i)
 
-#define _CHECK_EQ(name, lho, rho, fmt, eq_cmp)                           \
-  {                                                                      \
-    bool __assrt = eq_cmp(lho, rho);                                     \
-    char __name[MAX_TEST_DESC];                                          \
-    sprintf(__name, name ": " #lho " (" fmt ") == " #rho " (" fmt ")", (lho), \
-            (rho));                                                      \
-    _CHECK_1(__name, __assrt);                                           \
+int __print_check_eq_name(char* dest, size_t destsz, const char* fmt, ...);
+
+#define _CHECK_EQ(name, lho, rho, fmt, eq_cmp)                                 \
+  {                                                                            \
+    bool __assrt = eq_cmp(lho, rho);                                           \
+    char __name[MAX_TEST_DESC];                                                \
+    __print_check_eq_name(__name, MAX_TEST_DESC, EQ_FORMAT_STR(fmt), name, #lho, (lho), #rho, \
+                          (rho));                                              \
+    _CHECK_1(__name, __assrt);                                                 \
   }
 
 #define _EQ_COMP(lho, rho) ((lho) == (rho))
 #define _STREQ_COMP(lho, rho) (strcmp(lho, rho) == 0);
 
-#define CHECK_EQ_INT(name, lho, rho) _CHECK_EQ(name, lho, rho, "%d", _EQ_COMP)
-#define CHECK_EQ_UINT(name, lho, rho) _CHECK_EQ(name, lho, rho, "%u", _EQ_COMP)
-#define CHECK_EQ_CHAR(name, lho, rho) _CHECK_EQ(name, lho, rho, "'%c'", _EQ_COMP)
-#define CHECK_EQ_PTR(name, lho, rho) _CHECK_EQ(name, lho, rho, "%p", _EQ_COMP)
-#define CHECK_EQ_STR(name, lho, rho) _CHECK_EQ(name, lho, rho, "%s", _STREQ_CMP)
+// clang-format off
+#define EQ_FORMAT_STR(fmt) TESTING_FORMAT_TO_STR(%s: %s (fmt) == %s (fmt))
+
+#define CHECK_EQ_INT(name, lho, rho) _CHECK_EQ(name, lho, rho, %jd, _EQ_COMP)
+#define CHECK_EQ_UINT(name, lho, rho) _CHECK_EQ(name, lho, rho, %ju, _EQ_COMP)
+#define CHECK_EQ_CHAR(name, lho, rho) _CHECK_EQ(name, lho, rho, '%c', _EQ_COMP)
+#define CHECK_EQ_PTR(name, lho, rho) _CHECK_EQ(name, lho, rho, %p, _EQ_COMP)
+#define CHECK_EQ_STR(name, lho, rho) _CHECK_EQ(name, lho, rho, %s, _STREQ_CMP)
+// clang-format on
 
 #define TEST_CASE_FUNC(f, name_, file_, line_)     \
   test_function f;                                 \
@@ -97,5 +194,8 @@ bool run_test(test_function_ptr fp);
 // Execute several test functions
 bool run_tests(test_function_ptr* ts);
 
+TESTING_CLANG_SUPPRESS_WARNING_POP
+TESTING_MSVC_SUPPRESS_WARNING_POP
+TESTING_GCC_SUPPRESS_WARNING_POP
 
 #endif  // DBDS_TESTING_TEST_H_
